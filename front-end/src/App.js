6a249e858f5faf59025554c672a409d616917200
Antoine Buard <a.buard@laposte.net>
2022-12-15 08:27:06 +0100

    Ajout du header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
 
-function App() {}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ front-end/src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/argent-bank.png";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="main-nav">
+        <Link className="main-nav-logo" href="./index.html">
+          <img
+            className="main-nav-logo-image"
+            src={Logo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Link>
+        <div>
+          <Link className="main-nav-item" href="./sign-in.html">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
